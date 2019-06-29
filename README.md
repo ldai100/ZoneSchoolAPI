86bcc6a8e77cf7dcd10ea9ca45601b93e7c5c196
ldai100 <lidaivet@gmail.com>
2019-06-29 16:29:55 -0400

    updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# ZoneSchoolAPI
+# Zone School API
+
+Yao and I found out many real estate websites have inaccurate information about the zone schools of specific addresses.
+We want to make sure people are informed with correct zone schools of the houses they intend to buy/rent.
+
+## How it works
+This service is written using Spring Boot and gets it's data from https://www.schools.nyc.gov
+
+All data are available in public
+
+## How to run
+To run locally
+1) Do a maven build and simply run ZoneSchoolApiApplication.java
+2) Build the project and run ZoneSchoolAPI***.jar
--- src/main/java/com/yfcld/ZoneSchoolAPI/controller/AddressReceiver.java
+++ src/main/java/com/yfcld/ZoneSchoolAPI/controller/AddressReceiver.java
@@ -1,14 +1,22 @@
 package com.yfcld.ZoneSchoolAPI.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.yfcld.ZoneSchoolAPI.object.School;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class AddressReceiver {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/zoneSchool")
     public String zoneSchools(String address) {
-        return address;
+
+        return restTemplate.getForObject("https://www.schools.nyc.gov/find-a-school?address=" + address
+                + "&sort=nearest&ShowZonedSchools=1&ShowCurrentYear=1&mpp=36&school_borough=&pg=1&grade_levels=",
+                String.class);
     }
 }
