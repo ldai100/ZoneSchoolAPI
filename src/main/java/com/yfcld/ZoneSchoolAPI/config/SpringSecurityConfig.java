148fb4cb1430c991a37d80704a28896f7aef8ca8
yfc <chen.yaofeng@yahoo.com>
2019-07-06 14:33:10 -0400

    add basic spring security functionality

---
+++ src/main/java/com/yfcld/ZoneSchoolAPI/config/SpringSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.yfcld.ZoneSchoolAPI.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+}
